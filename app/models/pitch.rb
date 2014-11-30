0cae6b1f806c131948bb26799e7c9dfe36631f75
Zach Kissane <zkissane@zkissane.com>
2014-11-30 17:15:37 -0600

    pitch_type was named wrong, should have been pitch_result

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,8 +2,19 @@ class Pitch < ActiveRecord::Base
   belongs_to :pitcher
   belongs_to :game
   belongs_to :batter
+  belongs_to :pitch_result, :foreign_key => 'pitch_result'
   attr_accessible *column_names
   
+  def self.strikes
+    Pitch.joins(:pitch_result).where(:pitch_results => {:strike => true, :contact => false} )
+  end
   
+  def self.balls
+    Pitch.joins(:pitch_result).where(:pitch_results => {:strike => false, :swinging => false})
+  end
+  
+  def self.hits
+    Pitch.joins(:pitch_result).where(:pitch_results => {:in_play => true, :out => false} )
+  end
   
 end
