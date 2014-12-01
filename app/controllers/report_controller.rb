dc88cc1aa38d1a1810e604ee33ec048951b6f2bf
Zach Kissane <zkissane@zkissane.com>
2014-11-30 22:26:01 -0600

    committing stash for pitches per game work

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,5 +2,7 @@ class ReportController < ApplicationController
   
   def pitcher_performance
     @pitches = Pitch.where(:pitcher_id => params[:id])
+    @pitches_per_game = @pitches.group(:game_id).count.to_a
+    @pitches_per_game.insert(0, ["Game", "Pitches"])
   end
 end
