f0e8d54955807b1cd3b3f9bff6c664e753e4e277
Zach Kissane <zkissane@zkissane.com>
2014-11-30 23:18:00 -0600

    pitcher pitches per game works now

--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -1,5 +1,14 @@
 class Pitcher < ActiveRecord::Base
   self.primary_key = 'pitcher_id'
   has_many :pitches
+  
   attr_accessible :pitcher_id, :throws, :first_name, :last_name
+  
+  # #distinct and #uniq seem to be broken in this version of rails
+  #see https://github.com/rails/rails/issues/7399
+  #so work around with raw sql
+  #in a prod environment I'd try upgrading Rails to 4, not sure I have time at the moment
+  def distinct_games
+    Game.find_by_sql(["select distinct game_id from pitches where pitcher_id = ?", pitcher_id]).map(&:game_id)
+  end
 end
