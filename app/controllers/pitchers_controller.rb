5b4bfa13f552cbb4183041594f238875de0ef084
Zach Kissane <zkissane@zkissane.com>
2014-12-02 19:08:31 -0600

    search function for pitchers

--- app/controllers/pitchers_controller.rb
+++ app/controllers/pitchers_controller.rb
@@ -1,6 +1,12 @@
 class PitchersController < ApplicationController
   def index
-    @pitchers = Pitcher.order("pitcher_id").paginate(:page => params[:page])
+    if (params["search"])
+      search_term = params["search"] + '%'
+      @pitchers = Pitcher.where('pitcher_id like ? or first_name like ? or last_name like ?',
+        search_term, search_term, search_term).order("pitcher_id").paginate(:page => params[:page])
+    else
+      @pitchers = Pitcher.order("pitcher_id").paginate(:page => params[:page])
+    end
   end
   
   def show
